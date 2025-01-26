86bfd42bd65d4ed755a8c2b155ac35484c1bf90e
Christian Turner <124309572+Turner-Christian@users.noreply.github.com>
2025-01-25 23:24:22 -0500

    Second Commit

--- components/Accordion.jsx
+++ components/Accordion.jsx
@@ -1,11 +1,29 @@
-import React from "react";
-
+import { useState } from "react";
 
 const Accordion = ({ items }) => {
-    
+    const [isOpen, setisOpen] = useState(null);
+    console.log(isOpen)
+    const handle_collapse = (id) => {
+        if (isOpen === id){
+            setisOpen(null);
+        }
+        else{
+            setisOpen(id);
+        }
+    }
+
     return items.map((item, index) => (
-        <div className="item_container" key={index}>
-            <h3 className="header_box" >{item.title}</h3>
+        <div 
+            className="item_container" 
+            key={index}
+            >
+            <div className="drop_box">
+                <h3 
+                    className="header_box"
+                    onClick={() => handle_collapse(index)}
+                    >{item.title}</h3>
+                {isOpen === index && <p className="content_box">{item.content}</p>}
+            </div>
         </div>
     ));
 }
